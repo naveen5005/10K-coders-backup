ad1a7d7088bf46c2656d072b65c76d453fa39e5d
naveen@5005 <bellamnaveen314@gmail.com>
2022-12-15 07:45:58 +0530

    first commit

--- ReactJS/react-class-forms-person/src/App.js
+++ ReactJS/react-class-forms-person/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Component/Person';
 import UserDetail from './Component/UserDetail';
+import UniversityDetails from './Component/UniversityDetails';
 
 function App() {
   return (
     <div className="App">
       {/* <Person/> */}
-      <UserDetail/>
+      {/* <UserDetail/> */}
+      <UniversityDetails/>
     </div>
   );
 }
