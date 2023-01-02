e319981543ec3a9adc6f4fdba604a98d062fbde2
naveen5005 <100987924+naveen5005@users.noreply.github.com>
2023-01-02 16:52:08 +0530

    redux using class component

---
+++ ReactJS/05-react-redux-class-component/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const defaultState={
+  users : ["dog","cat","tiger","lion","leopord"]
+}
+export const addUserAction = () =>{
+  return{
+    type : "ADDUSER",
+    paload : "Gorilla"
+  }
+}
+export const deleteUserAction = (user) =>{
+  return{
+    type : "DELETEUSER",
+    paload : user
+  }
+}
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADDUSER":
+      let newUser = [...state.users];
+      newUser.push(action.paload);
+      return {...state,users:newUser}
+    case "DELETEUSER":
+      let updatedUsers = state.users.filter((user)=> user!==action.paload);
+      return {...state,users:updatedUsers}
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
